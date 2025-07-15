c646830ab4c8773b4223cddedada3321ec223a52
bagofbeanes <beanemoment@gmail.com>
2025-07-15 16:55:41 +0200

    Fixed typo

--- init.lua
+++ init.lua
@@ -5,5 +5,5 @@ local constants = require 'custom_playfield.util.constants'
 local skin_list = require 'custom_playfield.skinlist'
 
 -- Initialize a basic playfield
-local playfield_main = Playfield:new(skin_list.confict, 6):setLaneCount(4, constants.timingDefault):setExtraLaneCount(4, constants.timingDefault)
+local playfield_main = Playfield:new(skin_list.conflict, 6):setLaneCount(4, constants.timingDefault):setExtraLaneCount(4, constants.timingDefault)
 playfield_main.trackExtraController.active = CreateKey(0)
