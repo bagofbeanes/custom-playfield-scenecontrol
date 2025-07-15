efd376f6fa31801870fde66da0e91fe12f9fc489
bagofbeanes <beanemoment@gmail.com>
2025-07-15 09:38:05 +0200

    Changed folder structure

--- custom_playfield/main.lua
+++ custom_playfield/main.lua
@@ -1,11 +1,10 @@
 -- -- ------------ -- --
 -- [[              ]] --
 
-require 'custom_playfield.util'
-
-require 'custom_playfield.skinning.main'
+require 'custom_playfield.util.main'
 
 require 'custom_playfield.classes.controller'
+require 'custom_playfield.classes.skinning'
 require 'custom_playfield.classes.playfield'
 
 require 'custom_playfield.functions.lanecount'
