2ad6c7f99c4005262e7ce1ac3ec7974a8ec1da8d
bagofbeanes <beanemoment@gmail.com>
2025-08-05 02:09:02 +0200

    Added annotations to Context extension

--- custom_playfield/util/context_extension.lua
+++ custom_playfield/util/context_extension.lua
@@ -1,5 +1,19 @@
 ---@class beane.CustomPlayfield.ContextExtension : Context
 ---   @field skyInputHeight number|ValueChannel
+---   @field dropRate DropRateChannel
+---   @field globalOffset GlobalOffsetChannel
+---   @field currentScore CurrentScoreChannel
+---   @field currentCombo CurrentComboChannel
+---   @field currentTiming CurrentTimingChannel
+---   @field screenWidth ScreenWidthChannel
+---   @field screenHeight ScreenHeightChannel
+---   @field screenAspectRatio ProductChannel
+---   @field is16By9 ScreenIs16By9Channel
+---   @field isMirrorOn IsMirrorOnChannel
+---   @field beatLength fun(timingGroup: integer): ProductChannel
+---   @field bpm fun(timingGroup: integer): BPMChannel
+---   @field divisor fun(timingGroup: integer): DivisorChannel
+---   @field floorPosition fun(timingGroup: integer): FloorPositionChannel
 local context_extension = {}
 
 local mt = {
