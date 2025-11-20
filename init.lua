e0dc0d5a6390f913e6456eb45c7dfc5f6892cc3b
bagofbeanes <beanemoment@gmail.com>
2025-11-20 06:48:14 +0100

    Update init

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 require 'custom_playfield.main'
 
-local Context = require 'custom_playfield.util.context_extension'
-local constants = require 'custom_playfield.util.constants'
-local skin_list = require 'custom_playfield.skinlist'
+Context2 = require 'custom_playfield.util.context_extension'
+Constants = require 'custom_playfield.util.constants'
+SkinList = require 'custom_playfield.skinlist'
 
 -- Initialize a basic playfield
-local playfield_main = Playfield:new(skin_list.conflict, 6):setLaneCount(4, constants.timingDefault):setExtraLaneCount(4, constants.timingDefault)
+local playfield_main = Playfield:new(SkinList.conflict, 6):setLaneCount(4, Constants.timingDefault):setExtraLaneCount(4, Constants.timingDefault)
 playfield_main.trackExtraController.active = CreateKey(0)
